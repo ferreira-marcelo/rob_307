07cdddd2229c425b328bc66781d91840634126b7
fabianvija <farinconvija@gmail.com>
2018-11-17 22:39:22 +0100

    hardware for microblaze processor and arm added

---
+++ hardware_model/hw_arm_microblaze/hw_arm_microblaze.srcs/sources_1/bd/microblaze_arm/ip/microblaze_arm_lmb_bram_5/microblaze_arm_lmb_bram_5_stub.v
@@ -0,0 +1,36 @@
+// Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2018.2 (lin64) Build 2258646 Thu Jun 14 20:02:38 MDT 2018
+// Date        : Sat Nov 17 18:31:45 2018
+// Host        : fabian running 64-bit Ubuntu 16.04.5 LTS
+// Command     : write_verilog -force -mode synth_stub
+//               /home/fabian/Documents/ensta/deuxieme/rob307/rob_307/hardware_model/hw_arm_microblaze/hw_arm_microblaze.srcs/sources_1/bd/microblaze_arm/ip/microblaze_arm_lmb_bram_5/microblaze_arm_lmb_bram_5_stub.v
+// Design      : microblaze_arm_lmb_bram_5
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg484-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "blk_mem_gen_v8_4_1,Vivado 2018.2" *)
+module microblaze_arm_lmb_bram_5(clka, rsta, ena, wea, addra, dina, douta, clkb, rstb, enb, 
+  web, addrb, dinb, doutb, rsta_busy, rstb_busy)
+/* synthesis syn_black_box black_box_pad_pin="clka,rsta,ena,wea[3:0],addra[31:0],dina[31:0],douta[31:0],clkb,rstb,enb,web[3:0],addrb[31:0],dinb[31:0],doutb[31:0],rsta_busy,rstb_busy" */;
+  input clka;
+  input rsta;
+  input ena;
+  input [3:0]wea;
+  input [31:0]addra;
+  input [31:0]dina;
+  output [31:0]douta;
+  input clkb;
+  input rstb;
+  input enb;
+  input [3:0]web;
+  input [31:0]addrb;
+  input [31:0]dinb;
+  output [31:0]doutb;
+  output rsta_busy;
+  output rstb_busy;
+endmodule
